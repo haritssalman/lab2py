ebae3dd02ef2b76482296946d2a8d1447ca6f7c4
Salman <115677440+haritssalman@users.noreply.github.com>
2022-10-25 20:41:43 +0700

    Add files via upload

---
+++ latihan3.py
@@ -0,0 +1,12 @@
+# input nilai variable
+a = input("masukan nilai a:")
+b = input("masukan nilai b:")
+
+# cetak hasil operasi
+print ("hasil penggabungan {1}&{0}=%s".format(a,b) %(a+b))
+
+# konversi nilai
+a =int(a)
+b =int(b)
+print ("Hasil penjumlahan {1}+{0}=%s".format(a,b) %(a+b))
+print ("Hasil pembagian {1}/{0}=%s".format(a,b) %(a/b))    
